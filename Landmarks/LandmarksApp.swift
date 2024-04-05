6d11c78703a9a1a94ede5d8cac9a03591c66125f
Oliver Murray <creeperman69@icloud.com>
2024-04-05 09:32:37 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by OLIVER MURRAY on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
